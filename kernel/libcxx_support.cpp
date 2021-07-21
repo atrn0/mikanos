d0bf60797b7e3ebc58af154ced92cf199f28eaaa
atrn0 <14242642+atrn0@users.noreply.github.com>
2021-07-22 05:09:04 +0900

    day06c: finally move mouse cursor!!

---
+++ kernel/libcxx_support.cpp
@@ -0,0 +1,6 @@
+#include <cerrno>
+#include <new>
+
+std::new_handler std::get_new_handler() noexcept { return nullptr; }
+
+extern "C" int posix_memalign(void**, size_t, size_t) { return ENOMEM; }
--- kernel/pci.cpp
+++ kernel/pci.cpp
@@ -16,15 +16,14 @@ uint32_t MakeAddress(uint8_t bus, uint8_t device, uint8_t function,
 
 /** @brief devices[num_device] に情報を書き込み num_deviceをインクリメントする．
  */
-Error AddDevice(uint8_t bus, uint8_t device, uint8_t function,
-                uint8_t header_type) {
+Error AddDevice(const Device& device) {
   if (num_device == devices.size()) {
-    return Error::kFull;
+    return MAKE_ERROR(Error::kFull);
   }
 
-  devices[num_device] = Device{bus, device, function, header_type};
+  devices[num_device] = device;
   ++num_device;
-  return Error::kSuccess;
+  return MAKE_ERROR(Error::kSuccess);
 }
 
 Error ScanBus(uint8_t bus);
@@ -33,22 +32,21 @@ Error ScanBus(uint8_t bus);
  * もし PCI-PCI ブリッジなら，セカンダリバスに対し ScanBus を実行する
  */
 Error ScanFunction(uint8_t bus, uint8_t device, uint8_t function) {
+  auto class_code = ReadClassCode(bus, device, function);
   auto header_type = ReadHeaderType(bus, device, function);
-  if (auto err = AddDevice(bus, device, function, header_type)) {
+  Device dev{bus, device, function, header_type, class_code};
+  if (auto err = AddDevice(dev)) {
     return err;
   }
 
-  auto class_code = ReadClassCode(bus, device, function);
-  uint8_t base = (class_code >> 24) & 0xffu;  // 0b11111111u
-  uint8_t sub = (class_code >> 16) & 0xffu;   // 0b11111111u
-  if (base == 0x06u && sub == 0x04u) {
+  if (class_code.Match(0x06u, 0x04u)) {
     // standard PCI-PCI bridge
     auto bus_numbers = ReadBusNumbers(bus, device, function);
     uint8_t secondary_bus = (bus_numbers >> 8) & 0xffu;
     return ScanBus(secondary_bus);
   }
 
-  return Error::kSuccess;
+  return MAKE_ERROR(Error::kSuccess);
 }
 
 /** @brief 指定のデバイス番号の各ファンクションをスキャンする．
@@ -59,7 +57,7 @@ Error ScanDevice(uint8_t bus, uint8_t device) {
     return err;
   }
   if (IsSingleFunctionDevice(ReadHeaderType(bus, device, 0))) {
-    return Error::kSuccess;
+    return MAKE_ERROR(Error::kSuccess);
   }
 
   for (uint8_t function = 1; function < 8; ++function) {
@@ -71,7 +69,7 @@ Error ScanDevice(uint8_t bus, uint8_t device) {
     }
   }
 
-  return Error::kSuccess;
+  return MAKE_ERROR(Error::kSuccess);
 }
 
 /** @brief 指定のバス番号の各デバイスをスキャンする．
@@ -86,7 +84,7 @@ Error ScanBus(uint8_t bus) {
       return err;
     }
   }
-  return Error::kSuccess;
+  return MAKE_ERROR(Error::kSuccess);
 }
 }  // namespace
 
@@ -112,9 +110,14 @@ uint8_t ReadHeaderType(uint8_t bus, uint8_t device, uint8_t function) {
   return (ReadData() >> 16) & 0xffu;
 }
 
-uint32_t ReadClassCode(uint8_t bus, uint8_t device, uint8_t function) {
+ClassCode ReadClassCode(uint8_t bus, uint8_t device, uint8_t function) {
   WriteAddress(MakeAddress(bus, device, function, 0x08));
-  return ReadData();
+  auto reg = ReadData();
+  ClassCode cc;
+  cc.base = (reg >> 24) & 0xffu;
+  cc.sub = (reg >> 16) & 0xffu;
+  cc.interface = (reg >> 8) & 0xffu;
+  return cc;
 }
 
 uint32_t ReadBusNumbers(uint8_t bus, uint8_t device, uint8_t function) {
@@ -137,7 +140,7 @@ Error ScanAllBus() {
   }
 
   // several host bridges exist
-  for (uint8_t function = 1; function < 8; ++function) {
+  for (uint8_t function = 0; function < 8; ++function) {
     if (ReadVendorId(0, 0, function) == 0xffffu) {
       continue;
     }
@@ -146,6 +149,39 @@ Error ScanAllBus() {
     }
   }
 
-  return Error::kSuccess;
+  return MAKE_ERROR(Error::kSuccess);
+}
+
+uint32_t ReadConfReg(const Device& dev, uint8_t reg_addr) {
+  WriteAddress(MakeAddress(dev.bus, dev.device, dev.function, reg_addr));
+  return ReadData();
+}
+
+void WriteConfReg(const Device& dev, uint8_t reg_addr, uint32_t value) {
+  WriteAddress(MakeAddress(dev.bus, dev.device, dev.function, reg_addr));
+  WriteData(value);
+}
+
+WithError<uint64_t> ReadBar(Device& device, unsigned int bar_index) {
+  if (bar_index >= 6) {
+    return {0, MAKE_ERROR(Error::kIndexOutOfRange)};
+  }
+
+  const auto addr = CalcBarAddress(bar_index);
+  const auto bar = ReadConfReg(device, addr);
+
+  // 32 bit address
+  if ((bar & 4u) == 0) {
+    return {bar, MAKE_ERROR(Error::kSuccess)};
+  }
+
+  // 64 bit address
+  if (bar_index >= 5) {
+    return {0, MAKE_ERROR(Error::kIndexOutOfRange)};
+  }
+
+  const auto bar_upper = ReadConfReg(device, addr + 4);
+  return {bar | (static_cast<uint64_t>(bar_upper) << 32),
+          MAKE_ERROR(Error::kSuccess)};
 }
 }  // namespace pci
